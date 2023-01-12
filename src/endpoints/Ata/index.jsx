b7d58c1c1f2cb72149162db910369fae83bb0b3f
Ata Ahmad <ataahmad@Atas-MBP.lan>
2023-01-11 23:47:41 -0800

    Adds in basic headers and logic to present workday count

--- src/endpoints/Ata/index.jsx
+++ src/endpoints/Ata/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import './Ata.css';
 
 class Ata extends React.Component {
+
+    constructor(props) {
+        super(props);
+        const today = new Date();
+        const startDate = new Date("2020-03-23");
+        const diffTime = Math.abs(today - startDate);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        this.state = {
+            workDays: diffDays
+        }
+    }
 
     render() {
         return (
             <div>
-                <h1> This is Ata's page.</h1>
+                <div id="main">
+                    <h1>My name is Ata Ahmad</h1>
+                </div>
+                <h3 id="header">
+                    I've been a Vipershark for {this.state.workDays} days and counting!
+                </h3>
             </div>
         )
     }
