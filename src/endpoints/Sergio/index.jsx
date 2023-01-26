e6c3b6712fe5954e25993637bac78170449e6d43
Sergio Conde <sergio.condecarrasco@joinhoney.com>
2023-01-25 21:06:48 -0800

    adding bio and picture

--- src/endpoints/Sergio/index.jsx
+++ src/endpoints/Sergio/index.jsx
@@ -5,7 +5,28 @@ class Sergio extends React.Component {
     render() {
         return (
             <div>
-                <h1> This is Sergio's page.</h1>
+                <img src = '' class = 'profile-photo'></img>
+                <h1>Sergio F Conde</h1>
+                <h2>Biography</h2>
+                <p>Sergio has been a part of the Solutions and Integrations team at Honey since 2020. He currently serves as an integration lead.
+                    His responsibilities include:
+                    <ul>
+                        <li>Being a people manager to 5 integration specialists, which includes weekly 1-1s, coaching and advocating, and resolving any hurdles for them to do their best work.</li>
+                        <li>Overseeing the QA space within the organization, which includes managing a group of 4 specialists, setting and updating procedures, and ensuring SLA adherence for QA and POLO issues.</li>
+                        <li>Oversee the On-Call space within the organization, which includes managing On-Call schedules, setting and updating procedures, and insure the smooth operation of On-Call given that each week has a different specialist.</li>
+                        <li>Create processes and initiatives to achieve organizational goals.</li>
+                    </ul>
+                </p>
+                <h2>Resume:</h2> <div> Adding Link Here </div>
+                <h2>Technical Skills</h2>
+                <div>
+                    <p>More coming...</p>
+                </div>
+                <h2>Projects</h2>
+                <div>
+                    <p>More Coming... Thining something related to python or sql</p>
+                </div>
+                <h2></h2>
             </div>
         )
     }
